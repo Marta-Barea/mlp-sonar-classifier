745f5d7499dbe10f85f29f4faa0b784eee91c3ef
Marta Barea-Sepúlveda <79462299+Marta-Barea@users.noreply.github.com>
2025-06-11 17:15:56 +0200

    add weight initialization

--- src/build_model.py
+++ src/build_model.py
@@ -6,8 +6,10 @@ from tensorflow.keras.layers import Dense
 
 def build_model(input_dim: int, units: int = 60):
     model = Sequential()
-    model.add(Dense(units, input_dim=input_dim, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(units, input_dim=input_dim,
+              activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(1, activation='sigmoid',
+              kernel_initializer='glorot_uniform'))
     model.compile(loss='binary_crossentropy',
                   optimizer='adam', metrics=['accuracy'])
 
